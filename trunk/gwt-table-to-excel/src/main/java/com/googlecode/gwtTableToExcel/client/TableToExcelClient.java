f2e2b0c946351f8ca47ff4c757f88e28cf04c78a
wokier <wokier@0d0e9cf9-77c0-8b6d-95d9-7655e81e4fdf>
2013-01-04 00:15:52 +0000

    version 0.0.3 "gwt-visualization"

--- trunk/gwt-table-to-excel/src/main/java/com/googlecode/gwtTableToExcel/client/TableToExcelClient.java
+++ trunk/gwt-table-to-excel/src/main/java/com/googlecode/gwtTableToExcel/client/TableToExcelClient.java
@@ -1,22 +1,24 @@
 package com.googlecode.gwtTableToExcel.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.TableElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.HTMLTable;
 import com.google.gwt.user.client.ui.Hidden;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
+
 /**
  * Build a Form Panel in order to allow to download the table content in an excel format.<br>
  * Dont forget to add an iframe in your original html file :<br>
  * < iframe src="javascript:''" id="gwt-table-to-excel-target" class="invisible">< /iframe><br>
  * and to declare the sevlet in your web.xml :<br>
+ * 
  * @see com.googlecode.gwtTableToExcel.server.TableToExcelServlet
  * @author Francois Wauquier 'wokier'
  */
@@ -26,82 +28,125 @@ public class TableToExcelClient {
 
 	/**
 	 * Simple Constructor
+	 * 
 	 * @param table
 	 */
 	public TableToExcelClient(final HTMLTable table) {
-		this(table,new Label("Export"),"export");
+		this(table, new Label("Export"), "export");
 	}
-	
+
 	/**
 	 * Simple Constructor
+	 * 
 	 * @param table
 	 */
 	@SuppressWarnings("rawtypes")
 	public TableToExcelClient(final CellTable table) {
-		this(table.getElement(),new Label("Export"),"export");
+		this(table.getElement(), new Label("Export"), "export");
 	}
-	
+
+	/**
+	 * Simple Constructor
+	 * 
+	 * @param tableDomElement
+	 */
+	public TableToExcelClient(TableElement tableDomElement) {
+		this(tableDomElement, new Label("Export"), "export");
+	}
+
 	/**
 	 * Constructor with defined label text
+	 * 
 	 * @param table
 	 * @param labelText
 	 */
 	public TableToExcelClient(final HTMLTable table, String labelText) {
-		this(table.getElement(),new Label(labelText),"export");
+		this(table.getElement(), new Label(labelText), "export");
 	}
-	
+
 	/**
 	 * Constructor with defined label text
+	 * 
 	 * @param table
 	 * @param labelText
 	 */
 	@SuppressWarnings("rawtypes")
 	public TableToExcelClient(final CellTable table, String labelText) {
-		this(table.getElement(),new Label(labelText),"export");
+		this(table.getElement(), new Label(labelText), "export");
 	}
-	
+
+	/**
+	 * Constructor with defined label text
+	 * 
+	 * @param tableElement
+	 * @param labelText
+	 */
+	public TableToExcelClient(final TableElement tableElement, String labelText) {
+		this(tableElement, new Label(labelText), "export");
+	}
+
 	/**
 	 * Constructor with defined label text and fileName
+	 * 
 	 * @param table
 	 * @param labelText
 	 * @param fileName
+	 * @deprecated use {@link TableToExcelClient#TableToExcelClient(HTMLTable, Widget, String)} with
+	 *             Label as widget
 	 */
 	public TableToExcelClient(final HTMLTable table, String labelText, String fileName) {
-		this(table.getElement(),new Label(labelText),fileName);
+		this(table.getElement(), new Label(labelText), fileName);
 	}
-	
+
 	/**
 	 * Constructor with defined label text and fileName
+	 * 
 	 * @param table
 	 * @param labelText
 	 * @param fileName
+	 * @deprecated use {@link TableToExcelClient#TableToExcelClient(CellTable, Widget, String)} with
+	 *             Label as widget
 	 */
+	@Deprecated
 	@SuppressWarnings("rawtypes")
 	public TableToExcelClient(final CellTable table, String labelText, String fileName) {
-		this(table.getElement(),new Label(labelText),fileName);
+		this(table.getElement(), new Label(labelText), fileName);
 	}
-	
+
 	/**
 	 * Constructor with other widget (ex button)
+	 * 
 	 * @param table
 	 * @param exportWidget
 	 */
 	public TableToExcelClient(final HTMLTable table, HasClickHandlers exportWidget) {
-		this(table.getElement(),(Widget)exportWidget,"export");
+		this(table.getElement(), (Widget) exportWidget, "export");
 	}
-	
+
 	/**
 	 * Constructor with other widget (ex button)
+	 * 
 	 * @param table
 	 * @param exportWidget
 	 */
 	@SuppressWarnings("rawtypes")
 	public TableToExcelClient(final CellTable table, HasClickHandlers exportWidget) {
-		this(table.getElement(),(Widget)exportWidget,"export");
+		this(table.getElement(), (Widget) exportWidget, "export");
 	}
-	
+
 	/**
-	 * Constructor with ful options
+	 * Constructor with other widget (ex button)
+	 * 
+	 * @param tableElement
+	 * @param exportWidget
+	 */
+	public TableToExcelClient(final TableElement tableElement, HasClickHandlers exportWidget) {
+		this(tableElement, (Widget) exportWidget, "export");
+	}
+
+	/**
+	 * Constructor with full options
+	 * 
 	 * @param table
 	 * @param labelText
 	 * @param fileName
@@ -109,9 +154,10 @@ public class TableToExcelClient {
 	public TableToExcelClient(HTMLTable table, Widget exportWidget, String fileName) {
 		this(table.getElement(), exportWidget, fileName);
 	}
-	
+
 	/**
-	 * Constructor with ful options
+	 * Constructor with full options
+	 * 
 	 * @param table
 	 * @param labelText
 	 * @param fileName
@@ -121,9 +167,10 @@ public class TableToExcelClient {
 		this(table.getElement(), exportWidget, fileName);
 	}
 
-	private TableToExcelClient(final Element tableElement, Widget exportWidget, String fileName) {
-		formPanel.setAction(GWT.getModuleBaseURL()+"excel");
-//		formPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
+	private TableToExcelClient(final com.google.gwt.dom.client.Element tableElement,
+			Widget exportWidget, String fileName) {
+		formPanel.setAction(GWT.getModuleBaseURL() + "excel");
+		// formPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
 		formPanel.setMethod(FormPanel.METHOD_POST);
 		formPanel.addStyleName("gwt-table-to-excel-form");
 		FlowPanel flowPanel = new FlowPanel();
@@ -135,21 +182,32 @@ public class TableToExcelClient {
 		flowPanel.add(fileNameHidden);
 		flowPanel.add(exportWidget);
 		exportWidget.addStyleName("gwt-table-to-excel-exportWidget");
-		((HasClickHandlers)exportWidget).addClickHandler(new ClickHandler() {
+		((HasClickHandlers) exportWidget).addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				//lazy copy
+				// lazy copy
 				contentHidden.setValue(tableElement.getString());
 				formPanel.submit();
 			}
 		});
 	}
-	
+
 	/**
 	 * Give the created form
+	 * 
 	 * @return
+	 * @deprecated use {@link #getExportWidget()}
 	 */
 	public FormPanel build() {
 		return formPanel;
 	}
-	
+
+	/**
+	 * Give the created form
+	 * 
+	 * @return
+	 */
+	public FormPanel getExportWidget() {
+		return formPanel;
+	}
+
 }
